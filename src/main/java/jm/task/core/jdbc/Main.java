33e0a86527b1869436d42a0f78dc212872b99881
Uetal <uetal@me.com>
2020-09-08 13:13:48 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserService userService = new UserServiceImpl();
+        User user = new User("name","lastname", (byte) 4);
+
+        userService.createUsersTable();
+
+        userService.saveUser(user.getName(),user.getLastName(),user.getAge());
+        userService.saveUser(user.getName(),user.getLastName(),user.getAge());
+        userService.saveUser(user.getName(),user.getLastName(),user.getAge());
+        userService.saveUser(user.getName(),user.getLastName(),user.getAge());
+
+        System.out.println(userService.getAllUsers().toString());
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/dbusers?useSSL=false";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+
+    public Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USER,DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e ) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
